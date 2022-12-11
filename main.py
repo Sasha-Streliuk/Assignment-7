e8b0dc10b017ad7a144d6ab76962a0709ed01098
Sasha-Streliuk <alexstreliuk@gmail.com>
2022-12-11 16:09:11 +0200

    Basic stucture. Task 1

---
+++ main.py
@@ -0,0 +1,21 @@
+import argparse
+
+
+def country_year_validate(file_name, country, year):
+    pass
+
+
+def get_medalists(file_name, country, year):
+    pass
+
+
+def get_first_10(medalists):
+    pass
+
+
+def get_total_medals(medalists):
+    pass
+
+
+def save_to_file(file_name, first_10, total_medals):
+    pass
